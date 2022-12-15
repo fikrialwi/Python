51289e62af41376403d02d45d0673ff83701ada4
fikrialwi <dzulfikrialwim@gmail.com>
2022-12-15 07:01:38 +0700

    FInish

--- Fun Math/list_anime.py
+++ Fun Math/list_anime.py
@@ -5,11 +5,20 @@ def data_anime(path_save, path_anime):
 
     file.seek(0, os.SEEK_END)
     file.write(f"directory->{path_anime}:")
-    txt = ""
+    txt = "\n"
     for i in list_anime:
-        txt += f"{i}/n"
+        txt += f"{i}\n"
     
-    file.seek(4, os.SEEK_END)
+    file.seek(0, os.SEEK_END)
     file.write(txt)
     file.close()
+    file = open(path_save, "r")
+    for i in file:
+        print(i)
+
+path_save = "C://Users/ADMIN/Desktop/Daftar Anime.txt"
+path_anime = "E://Anime"
+path_anime2= "K://My Drive/Anime"
+data_anime(path_save=path_save, path_anime=path_anime)
+
 
